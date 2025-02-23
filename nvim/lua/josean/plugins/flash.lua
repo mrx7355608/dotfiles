142b970ac51ab849fb0746616622caa470e40516
mrx7355608 <mrx7355608@gmail.com>
2025-02-23 17:51:28 +0500

    enabled flash.toggle() globally and added a keybinding to toggle it manually

--- nvim/lua/josean/plugins/flash.lua
+++ nvim/lua/josean/plugins/flash.lua
@@ -1,27 +1,30 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    { "<c-s>", mode = { "n" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        { "<c-s>", mode = { "n" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
     config = function()
         local flash = require("flash")
         flash.setup({
             modes = {
                 search = {
-                    enabled =true,
+                    enabled = true,
                 }
-            }
+            },
         })
 
         local keymap = vim.keymap;
 
-        keymap.set({"n","x", "o"}, "sf", function() 
-            flash.jump({ search = { forward = true, wrap = false, multi_window = false }}) 
+        keymap.set({ "n", "x", "o" }, "sf", function()
+            flash.jump({ search = { forward = true, wrap = false, multi_window = false } })
         end, { desc = "Flash search for word (forwards)" })
-        keymap.set({"n","x", "o"}, "sb", function() 
-            flash.jump({ search = { forward = false, wrap = false, multi_window = false }}) 
+        keymap.set({ "n", "x", "o" }, "sb", function()
+            flash.jump({ search = { forward = false, wrap = false, multi_window = false } })
         end, { desc = "Flash search for word (backwards)" })
+        keymap.set("n", "<leader>tf", function()
+            flash.toggle()
+        end, { desc = "Toggle flash with search" })
     end
 }
