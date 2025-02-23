d2f7603c4bf8839630a4c74eff0163e911a355ef
mrx7355608 <mrx7355608@gmail.com>
2025-02-23 16:47:59 +0500

    setup flash.nvim

--- nvim/lua/josean/plugins/flash.lua
+++ nvim/lua/josean/plugins/flash.lua
@@ -2,7 +2,6 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   opts = {},
-  -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -10,4 +9,17 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
+    config = function()
+        local keymap = vim.keymap;
+        local flash = require("flash")
+
+        flash.toggle() -- integrate with / ? searches
+
+        keymap.set({"n","x", "o"}, "<leader>sf", function() 
+            flash.jump({ search = { forward = true, wrap = false, multi_window = false }}) 
+        end, { desc = "Flash search for word (forwards)" })
+        keymap.set({"n","x", "o"}, "<leader>sb", function() 
+            flash.jump({ search = { forward = false, wrap = false, multi_window = false }}) 
+        end, { desc = "Flash search for word (backwards)" })
+    end
 }
