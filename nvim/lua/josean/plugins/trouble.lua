a0553d0e19792196dd7a1caf0bf9280040c978be
mrx7355608 <mrx7355608@gmail.com>
2025-03-04 04:37:13 +0500

    configured trouble.nvim keymaps

--- nvim/lua/josean/plugins/trouble.lua
+++ nvim/lua/josean/plugins/trouble.lua
@@ -6,10 +6,10 @@ return {
   },
   cmd = "Trouble",
   keys = {
-    { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
+    { "<leader>xt", "<cmd>TroubleToggle todo<CR>", desc = "Open todos in trouble" },
   },
 }
