5213e2e9c45197533901529cb095e14aa9d6e502
mrx7355608 <mrx7355608@gmail.com>
2025-03-16 06:24:27 +0500

    configuring hyprland

--- nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/lua/josean/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("NeoSolarized").setup({
-        transparent = false,
+        transparent = true,
         styles = {
           functions = { bold = false },
         },
